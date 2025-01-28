3503186e6bb48363c0fe724e58c7a1f0a6f58187
pythonthm <pythonthm@gmail.com>
2025-01-28 16:05:01 +0900

    photo

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ app = Flask(__name__, static_folder='./static/')
 def happy_new_year():
     return render_template("main.html")
 
+@app.route('/senkyo')
+def snekyo():
+    return render_template('senkyo.html')
+
 
 if __name__ == "__main__":
     app.run(debug = True,port=8888, threaded = True)
---
+++ static/main.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+  location.href = "./";
+  alert("HOMEにもどります");
+});
